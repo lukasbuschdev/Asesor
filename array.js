f1c767ba0942fa47d311ea9e0232371e153152ed
Lukas Busch <lukas.busch1996@gmx.at>
2023-03-08 15:26:55 +0100

    home-cards-subjects minor bugfixes

--- array.js
+++ array.js
@@ -1,18 +1,18 @@
 let homeCards = [
     {
-        title : "Negocios",
-        image : "img/category1.jpg",
-        subjects : ['1', '2', '3', '4'],
-    },
-    {
         title : "Diseño",
         image : "img/category2.jpeg",
-        subjects : ['5', '6', '7', '8'],
+        subjects : ['Animación 2D I', 'Animación 2D II', 'Animación 3D I', 'Producción y retoque digital', 'Programación multimedia I', 'Motion graphics y efectos visuales', 'Modelado digital'],
     },
     {
         title : "Ciencias de la salud",
         image : "img/category3.jpg",
-        subjects : ['9', '2', '3', '4'],
+        subjects : ['Bioquímica', 'Biología del desarollo', 'Biofísico', 'Química orgánica', 'Metodología de investigación para la salud'],
+    },
+    {
+        title : "Negocios",
+        image : "img/category1.jpg",
+        subjects : ['1', '2', '3', '4'],
     },
     {
         title : "Ciencias júridicas y sociales",
