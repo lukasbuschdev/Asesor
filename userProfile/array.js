e4c4ba60b6406bb4fbc7252251ee3f6e3c98cbd7
Lukas Busch <lukas.busch1996@gmx.at>
2023-03-08 21:12:04 +0100

    added user profile

---
+++ userProfile/array.js
@@ -0,0 +1,13 @@
+let userInfo = [
+    {
+        userImage : '/img/user-image.jpg',
+        name : 'Lukas',
+        description : 'Soy un estudiante que le encanta las matemáticas y ensenar a los demás. Además, soy paciente y si el tema se te complica mucho, haré todo lo posible hasta que lo entiendas.',
+        university : 'Anáhuac Mayab',
+        semester : 6,
+        subject : 'Economía',
+        grade : 9.3,
+        usedAsesories : 4,
+        location : 'Mérida'
+    }
+];
---
+++ userProfile/user.js
@@ -0,0 +1,65 @@
+function renderUserProfile() {
+    let container = document.getElementById('user-container');
+    container.innerHTML = '';
+
+    for (let i = 0; i < userInfo.length; i++) {
+        container.innerHTML += userProfileTemplate(i);
+    }
+}
+
+function userProfileTemplate(i) {
+    return `
+        <div id="user-profile${i}" class="user-profile">
+            ${userImage(i)}
+            ${userInformation(i)}
+        </div>
+    `;
+}
+
+function userImage(i) {
+    return `
+        <div class="image">
+            <img class="user-profile-img" src="${userInfo[i].userImage}">
+
+            <div class="user-profile-name">
+                <p>${userInfo[i].name}</p>
+            </div>
+        </div>
+    `;
+}
+
+function userInformation(i) {
+    return `
+        <div class="information">
+            <div class="description">
+                <p>Descripción</p>
+                <span>${userInfo[i].description}</span>
+            </div>
+
+            <div class="text">
+                <p>Asignatura</p>
+                <p>${userInfo[i].subject}</p>
+            </div>
+
+            <div class="text">
+                <p>Promedio</p>
+                <p>${userInfo[i].grade}</p>
+            </div>
+
+            <div class="text">
+                <p>Universidad</p>
+                <p>${userInfo[i].university}</p>
+            </div>
+
+            <div class="text">
+                <p>Accesorios tomados</p>
+                <p>${userInfo[i].usedAsesories}</p>
+            </div>
+
+            <div class="text">
+                <p>Ubicación</p>
+                <p>${userInfo[i].location}</p>
+            </div>
+        </div>
+    `;
+}
